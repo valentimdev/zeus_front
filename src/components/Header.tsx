2e52807fddfdeb58e57ca6bfcd486106e4e7bdac
valentimdev <valentimdesenvolvedor@gmail.com>
2025-08-28 13:04:43 -0300

    possivel final build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import '../style/Header.css';
 import { Link } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 import RegistrarCompra from './RegistrarCompra';
+import logo from '../assets/images/logo.png';
 
 const Header: React.FC<{ onCompraCriada: () => void }> = ({
   onCompraCriada,
@@ -14,7 +15,8 @@ const Header: React.FC<{ onCompraCriada: () => void }> = ({
     >
       <nav className=" container mx-auto flex gap-[15px] m-[20px] p-2.5 items-center w-full justify-between">
         <div className="flex gap-[20px] items-center justify-center">
-          <h1 className="font-bold text-2xl text-white">Patas na Conta</h1>
+          {/* <h1 className="font-bold text-2xl text-white">Patas na Conta</h1> */}
+          <img src={logo} alt="Logo" className="h-13 w-70" />
           {/* <Link to="/">
             Inicio
           </Link> */}
